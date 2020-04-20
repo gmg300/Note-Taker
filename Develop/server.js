ab3877aab8f8ef3c2ab3725c86ebf210b329aee7
Garrett <garrettgassensmith@gmail.com>
2020-04-20 15:36:10 -0500

    Added comments to server code and re-arranged user feedback

--- Develop/server.js
+++ Develop/server.js
@@ -14,55 +14,48 @@ const PORT = 3000;
 // Data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 // Static file serving
-app.use("/assets", express.static(path.join(__dirname,"/public","/assets")));
-app.use(express.static("public"));
+app.use("/assets", express.static(path.join(__dirname,"/public","/assets"))); // serves css and js files
+app.use(express.static("public")); // serves html files
 
 
 // ROUTES
 // ====================================================
-// Get routes
+// Get route
 app.get("/api/notes", function(req, res) {
-  let notes = fs.readFileSync("db/db.json","utf8");
+  let notes = fs.readFileSync("db/db.json", "utf8");
   notes = JSON.parse(notes);
   return res.json(notes);
 });
-
-// Post routes
+// Post route
 app.post("/api/notes", function(req, res) {
-  let newNote = req.body; // req.body = JSON from user input
-  console.log(chalk.cyan("Saving new note..."));
+  let newNote = req.body; // get new note data
   let notes = JSON.parse(fs.readFileSync("db/db.json","utf8"));
   notes.push(newNote); // add new note to notes array
   notes.map((note, i) => { // reset unique IDs for each note
     note.id = i + 1;
   });
-  fs.writeFileSync("db/db.json", JSON.stringify(notes, null, 2));
+  console.log(chalk.cyan("---- New note saved! ----")); // user feedback
+  fs.writeFileSync("db/db.json", JSON.stringify(notes, null, 2)); // Re-write file
   res.json(newNote);
 });
-
-// Delete routes
+// Delete route
 app.delete("/api/notes/:id", function(req, res) {
-  let noteNumber = +req.params.id;
-  console.log(chalk.red(`Deleting note ${noteNumber}...`));
+  let noteNumber = +req.params.id; // get note id and change to integer
   let notes = JSON.parse(fs.readFileSync("db/db.json", "utf8"));
-  // let deletedNote = notes.filter(note => {
-  //   console.log(note); // filter deleted note from array
-  //   return note.id === noteNumber;  
-  // });
-  // console.log(deletedNote);
   notes = notes.filter(note => { // filter deleted note from array
     return note.id !== noteNumber;
   });
   notes.map((note, i) => { // reset unique IDs for each note
     note.id = i + 1;
   });
-  fs.writeFileSync("db/db.json", JSON.stringify(notes, null, 2));
+  console.log(chalk.red(`---- Note ${noteNumber} deleted! ----`)); // user feedback
+  fs.writeFileSync("db/db.json", JSON.stringify(notes, null, 2)); // Re-write file
   res.json(noteNumber);
 });
+
 // START SERVER
 // =============================================================
 app.listen(PORT, function() {
-  console.log(chalk.green("App listening on PORT " + PORT));
+  console.log(chalk.green(`---- App listening on PORT ${PORT} ----`));
 });
